2176915b1da61ea4c0f52937d997d88d49564eb5
Maxime Daniel <root@maxux.net>
2023-05-08 14:14:51 +0200

    config: add sample

---
+++ config.sample.py
@@ -0,0 +1,11 @@
+config = {
+    'debug': True,
+    'listen': '0.0.0.0',
+    'port': 7811,
+
+    'db-server': '127.0.0.1',
+    'db-user': 'feedback',
+    'db-pass': '',
+    'db-name': 'feedback',
+}
+
