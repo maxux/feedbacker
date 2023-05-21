276db361e469b365b6c19d66726137d14a5e7b2d
Maxime Daniel <root@maxux.net>
2023-05-21 12:10:12 +0200

    core: supports greeting message and primary color

--- feedbacksrv.py
+++ feedbacksrv.py
@@ -32,7 +32,7 @@ def before_request_handler():
 @app.route('/fill/<gid>', methods=['GET', 'POST'])
 def feedback_fill(gid):
     cursor = g.db.cursor()
-    query = "SELECT name FROM groups WHERE id = %s"
+    query = "SELECT name, greeting, color FROM groups WHERE id = %s"
     cursor.execute(query, (gid))
 
     row = cursor.fetchone()
@@ -42,6 +42,8 @@ def feedback_fill(gid):
     content = {
         "gid": gid,
         "name": row[0],
+        "greeting": row[1],
+        "primary": row[2]
     }
 
     if request.method == "POST":
@@ -80,6 +82,36 @@ def feedback_login_credentials(gid, token):
     }
 
     return render_template("manage.html", **content)
+
+@app.route('/login/<gid>/<token>/settings', methods=['GET', 'POST'])
+def feedback_login_credentials_settings(gid, token):
+    if request.method == "POST":
+        greeting = request.form['greeting']
+        color = request.form['color']
+
+        cursor = g.db.cursor()
+        query = "UPDATE groups SET greeting = %s, color = %s WHERE id = %s AND token = %s"
+        cursor.execute(query, (greeting, color, gid, token))
+
+        return redirect(f"/login/{gid}/{token}")
+
+    cursor = g.db.cursor()
+    query = "SELECT name, greeting, color FROM groups WHERE id = %s AND token = %s"
+    cursor.execute(query, (gid, token))
+
+    row = cursor.fetchone()
+    if row == None:
+        return render_template("login.html", error="login")
+
+    content = {
+        "name": row[0],
+        "id": gid,
+        "token": token,
+        "greeting": row[1],
+        "color": row[2]
+    }
+
+    return render_template("settings.html", **content)
 
 @app.route('/login', methods=['GET', 'POST'])
 def feedback_login():
